34512531f146ae9a7aed3e1b53b24087acc4a080
Jean Carlo <jean.carlo.jc1@gmail.com>
2018-06-24 12:23:21 -0300

    Selectors refactored

--- src/test/java/PageObjects/CustomerIndex.java
+++ src/test/java/PageObjects/CustomerIndex.java
@@ -11,25 +11,38 @@ import testSuite.TestBase;
 
 public class CustomerIndex extends TestBase{
 	
+	private static String themeSelector = "switch-version-select";
+	private static String newCustomerButtonSelector = ".header-tools div.t5 a.btn-default";
+	private static String searchCustomerButtonSelector = "a.search-button";
+	private static String searchCustomerFieldSelector = "search-input";
+	private static String checkAllCustomerSelector = "select-all-none";
+	private static String deleteButtonSelector = "delete-selected-button";
+	private static String deleteConfirmationMessageSelector = "p.alert-delete-multiple-one";
+	private static String deleteConfirmationButtonSelector = "delete-multiple-confirmation-button";
+	private static String successDeletedMessageSelector = "div.alert-success span p";
+	private static String deletefeedbackMessage = "Your data has been successfully deleted from the database.";
+	private static String deleteConfirmationMessage = "Are you sure that you want to delete this 1 item?";
+	private static String themeValue = "bootstrap_theme_v4";
+	
   public CustomerIndex(WebDriver driver) {
 	  super(driver);
   }
   
   public CustomerIndex changeTheme() {
-	  WebElement select = getDriver().findElement(By.id("switch-version-select"));
+	  WebElement select = getDriver().findElement(By.id(themeSelector));
 	  Select bootstrapVersion = new Select(select);
-	  bootstrapVersion.selectByValue("bootstrap_theme_v4");
+	  bootstrapVersion.selectByValue(themeValue);
 	  return this;
   }
   
   public NewCustomer clickOnNewCustomerButton() {
-	  getDriver().findElement(By.cssSelector(".header-tools div.t5 a.btn-default")).click();
+	  getDriver().findElement(By.cssSelector(newCustomerButtonSelector)).click();
 	  return new NewCustomer(getDriver());
   }
   
   public CustomerIndex SearchCustomerByName(String name) {
-	  getDriver().findElement(By.cssSelector("a.search-button")).click();
-	  WebElement searchField = getDriver().findElement(By.className("search-input"));
+	  getDriver().findElement(By.cssSelector(searchCustomerButtonSelector)).click();
+	  WebElement searchField = getDriver().findElement(By.className(searchCustomerFieldSelector));
 	  searchField.sendKeys(name);
 	  searchField.sendKeys(Keys.ENTER);
 	  waitAfterSearch(name);
@@ -38,32 +51,32 @@ public class CustomerIndex extends TestBase{
   }
   
   public CustomerIndex selectAllCustomerToBeDeleted() {
-	  WebElement checkBox = getDriver().findElement(By.className("select-all-none"));
+	  WebElement checkBox = getDriver().findElement(By.className(checkAllCustomerSelector));
 	  checkBox.click();
-	  WebElement deleteButton = getDriver().findElement(By.className("delete-selected-button"));
+	  WebElement deleteButton = getDriver().findElement(By.className(deleteButtonSelector));
 	  deleteButton.click();
 	  
 	  return this;
   }
   
   public CustomerIndex validateDeleteConfirmationMessage() {
-	  WebElement message = getDriver().findElement(By.cssSelector("p.alert-delete-multiple-one"));
+	  WebElement message = getDriver().findElement(By.cssSelector(deleteConfirmationMessageSelector));
 	  waitElementToBeVisible(message);
-	  Assert.assertTrue(message.getText().contains("Are you sure that you want to delete this 1 item?"));
+	  Assert.assertTrue(message.getText().contains(deleteConfirmationMessage));
 	  
 	  return this;
   }
   
   public CustomerIndex deleteCustomer() {
-	  getDriver().findElement(By.className("delete-multiple-confirmation-button")).click();
+	  getDriver().findElement(By.className(deleteConfirmationButtonSelector)).click();
 	  waitAfterDelete();
 	  
 	  return this;
   }
   
   public CustomerIndex validateDeletedMessageSuccessfully() {
-	  WebElement message = getDriver().findElement(By.cssSelector("div.alert-success span p"));
-	  Assert.assertTrue(message.getText().contains("Your data has been successfully deleted from the database."));
+	  WebElement message = getDriver().findElement(By.cssSelector(successDeletedMessageSelector));
+	  Assert.assertTrue(message.getText().contains(deletefeedbackMessage));
 	  
 	  return this;
   }
--- src/test/java/PageObjects/NewCustomer.java
+++ src/test/java/PageObjects/NewCustomer.java
@@ -10,32 +10,49 @@ import junit.framework.Assert;
 import testSuite.TestBase;
 
 public class NewCustomer extends TestBase {
-
+	
+	private static String customerNameFieldSelector = "field-customerName";
+	private static String contactLastNameFieldSelector = "field-contactLastName";
+	private static String contactFirstNameFieldSelector = "field-contactFirstName";
+	private static String phoneFieldSelector = "field-phone";
+	private static String addressLine1FieldSelector = "field-addressLine1";
+	private static String addressLine2FieldSelector = "field-addressLine2";
+	private static String cityFieldSelector = "field-city";
+	private static String stateFieldSelector = "field-state";
+	private static String postalCodeFieldSelector = "field-postalCode";
+	private static String countryFieldSelector = "field-country";
+	private static String creditLimitFieldSelector = "field-creditLimit";
+	private static String fromEmployeeSelector = "field_salesRepEmployeeNumber_chosen";
+	private static String fromEmployeeFieldSelector = "#field_salesRepEmployeeNumber_chosen div.chosen-search input";
+	private static String saveButtonSelector = "form-button-save";
+	private static String successFeedbackSelector = "report-success";
+	private static String backToCustomerLinkSelector = "#report-success a:nth-child(2)";
+	private static String customerSuccessfullyAddedMessage = "Your data has been successfully stored into the database";
+	
 	public NewCustomer(WebDriver driver) {
 		super(driver);
 	}
 	
 	public NewCustomer setCustomerInformation(Customer customer) {
-		getDriver().findElement(By.id("field-customerName")).sendKeys(customer.getName());
-		getDriver().findElement(By.id("field-contactLastName")).sendKeys(customer.getLastName());
-		getDriver().findElement(By.id("field-contactFirstName")).sendKeys(customer.getContactFirstName());
-		getDriver().findElement(By.id("field-phone")).sendKeys(customer.getPhone());
-		getDriver().findElement(By.id("field-addressLine1")).sendKeys(customer.getAddressLine1());
-		getDriver().findElement(By.id("field-addressLine2")).sendKeys(customer.getAddressLine2());
-		getDriver().findElement(By.id("field-city")).sendKeys(customer.getCity());
-		getDriver().findElement(By.id("field-state")).sendKeys(customer.getState());
-		getDriver().findElement(By.id("field-postalCode")).sendKeys(customer.getPostalCode());
-		getDriver().findElement(By.id("field-country")).sendKeys(customer.getCountry());
+		getDriver().findElement(By.id(customerNameFieldSelector)).sendKeys(customer.getName());
+		getDriver().findElement(By.id(contactLastNameFieldSelector)).sendKeys(customer.getLastName());
+		getDriver().findElement(By.id(contactFirstNameFieldSelector)).sendKeys(customer.getContactFirstName());
+		getDriver().findElement(By.id(phoneFieldSelector)).sendKeys(customer.getPhone());
+		getDriver().findElement(By.id(addressLine1FieldSelector)).sendKeys(customer.getAddressLine1());
+		getDriver().findElement(By.id(addressLine2FieldSelector)).sendKeys(customer.getAddressLine2());
+		getDriver().findElement(By.id(cityFieldSelector)).sendKeys(customer.getCity());
+		getDriver().findElement(By.id(stateFieldSelector)).sendKeys(customer.getState());
+		getDriver().findElement(By.id(postalCodeFieldSelector)).sendKeys(customer.getPostalCode());
+		getDriver().findElement(By.id(countryFieldSelector)).sendKeys(customer.getCountry());
 		this.selectFromEmployeer(customer.getFromEmployeer());
-		getDriver().findElement(By.id("field-creditLimit")).sendKeys(customer.getCreditLimit());
+		getDriver().findElement(By.id(creditLimitFieldSelector)).sendKeys(customer.getCreditLimit());
 		
 		return this;
 	}
 	
 	public NewCustomer selectFromEmployeer(String value) {
-		getDriver().findElement(By.id("field_salesRepEmployeeNumber_chosen")).click();
-		WebElement fakeSelect =  getDriver()
-				.findElement(By.cssSelector("#field_salesRepEmployeeNumber_chosen div.chosen-search input"));
+		getDriver().findElement(By.id(fromEmployeeSelector)).click();
+		WebElement fakeSelect =  getDriver().findElement(By.cssSelector(fromEmployeeFieldSelector));
 		fakeSelect.sendKeys(value);
 		fakeSelect.sendKeys(Keys.TAB);
 				
@@ -43,23 +60,22 @@ public class NewCustomer extends TestBase {
 	}
 	
 	public NewCustomer saveCustomerForm() {
-		getDriver().findElement(By.id("form-button-save")).click();
+		getDriver().findElement(By.id(saveButtonSelector)).click();
 		
 		return this;
 	}
 	
 	public NewCustomer validateCustumerCreation() {
-		WebElement successFeedback = getDriver().findElement(By.id("report-success"));
+		WebElement successFeedback = getDriver().findElement(By.id(successFeedbackSelector));
 		waitElementToBeVisible(successFeedback);
 		
-		Assert.assertTrue(successFeedback.getText()
-				.contains("Your data has been successfully stored into the database"));
+		Assert.assertTrue(successFeedback.getText().contains(customerSuccessfullyAddedMessage));
 		
 		return this;
 	}
 	
 	public CustomerIndex goBackToCustomerList() {
-		WebElement backButton = getDriver().findElement(By.cssSelector("#report-success a:nth-child(2)"));
+		WebElement backButton = getDriver().findElement(By.cssSelector(backToCustomerLinkSelector));
 		waitElementToBeVisible(backButton);
 		backButton.click();
 		
